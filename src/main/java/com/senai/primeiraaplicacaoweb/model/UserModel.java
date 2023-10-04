a80151964d4c6d7aecdf14b3507cf15a577f3612
stefanny isabele <stefannynunes238.gmail.com>
2023-10-04 17:00:54 -0300

    projeto java

---
+++ src/main/java/com/senai/primeiraaplicacaoweb/model/UserModel.java
@@ -0,0 +1,25 @@
+package com.senai.primeiraaplicacaoweb.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private String email;
+    @Column
+    private String tel;
+    @Column(columnDefinition = "date")
+    private String date;
+}
